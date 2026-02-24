0ffa9e0c0494d59fbf8ad74651c61ab9bcf5829f
Atsushi Ogawa <atsushi.ogawa@gmail.com>
2026-02-24 12:40:14 +0900

    Initial Commit (copy from psqledit_psqlgrid)

---
+++ src/libs/pglib/pglib.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025, Atsushi Ogawa
+ * All rights reserved.
+ *
+ * This software is licensed under the BSD License.
+ * See the LICENSE_BSD file for details.
+ */
+
+#ifndef PGLIB_INCLUDE
+#define PGLIB_INCLUDE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct pg_session_st *	HPgSession;
+typedef struct pg_dataset_st *	HPgDataset;
+
+#include "pgapi.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PGLIB_INCLUDE */
+
